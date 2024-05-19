66787b0a78c20d49ab0f7f6126150dbcea99ef86
Italo Castillo <italogitdoe@gmail.com>
2024-05-20 00:14:44 +0200

    useState

--- projects/00-hello/src/App.jsx
+++ projects/00-hello/src/App.jsx
@@ -1,34 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const [name, setName] = useState("unknown");
   const format = (userName) => `@${userName}`;
+  const jonDoe = {
+    isFollowing: false,
+    userName: "jonDoe",
+    formatUsername: format,
+  };
+  const handleClickBtn = () => {
+    setName('doeJones')
+  };
   return (
     <section className="app">
-      <TwitterFollowCard
-        userName="jonny00"
-        name="Jon mighty"
-        isFollowing
-        formatUsername={format}
-      />
-      <TwitterFollowCard
-        userName="eli0"
-        name="Jenny Mcfubber"
-        isFollowing
-        formatUsername={format}
-      />
-      <TwitterFollowCard
-        userName="light"
-        name="Manuel Klein"
-        isFollowing={false}
-        formatUsername={format}
-      />
-      <TwitterFollowCard
-        userName="dark"
-        name="Luke Juss"
-        isFollowing={false}
-        formatUsername={format}
-      />
+      <TwitterFollowCard userName="jonny00" isFollowing formatUsername={format}>
+        Jonny Mclera
+      </TwitterFollowCard>
+      <TwitterFollowCard userName="eli0" isFollowing formatUsername={format}>
+        Lenny Notte
+      </TwitterFollowCard>
+      <TwitterFollowCard isFollowing={false} formatUsername={format}>
+        Jessy Kolrte
+      </TwitterFollowCard>
+      <TwitterFollowCard userName={name} isFollowing={false} formatUsername={format}>
+        Jane Park
+      </TwitterFollowCard>
+      <TwitterFollowCard {...jonDoe}>Jon Doe</TwitterFollowCard>
+      <button onClick={handleClickBtn}>Change Name</button>
     </section>
   );
 }
--- projects/00-hello/src/TwitterFollowCard.jsx
+++ projects/00-hello/src/TwitterFollowCard.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 
-export function TwitterFollowCard({formatUsername, userName, name, isFollowing}) {
- 
-    return (
-      <article className='tw-followCard' /*style={{ display: "flex", color: "white", alignItems: "center" }}*/>
-        <header className='tw-followCard-header '>
-          <img
-          className='tw-followCard-avatar'
-            alt="avatar"
-            src={`https://unavatar.io/${userName}`}
-          />
-          <div className='tw-followCard-info'>
-            <strong>{name}</strong>
-            <span>{formatUsername(userName)}</span>
-          </div>
-        </header>
-        <aside>
-          <button className='tw-followCard-btn' >
-            Follow
-            </button>
-        </aside>
-      </article>
-    );
-  }
-  
+export function TwitterFollowCard({
+  children,
+  formatUsername,
+  userName = "unknown",
+}) {
+  //const state = useState(false);
+  // const isFollowing = state[0],  setIsFollowing = state[1]
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const followText = isFollowing ? "Following" : "Follow";
+  const followBtnClassName = isFollowing
+    ? "tw-followCard-btn is-following"
+    : "tw-followCard-btn";
+
+  const handleClickBtn = () => {
+    setIsFollowing(!isFollowing);
+  };
+
+  return (
+    <article
+      className="tw-followCard"
+      /*style={{ display: "flex", color: "white", alignItems: "center" }}*/
+    >
+      <header className="tw-followCard-header ">
+        <img
+          className="tw-followCard-avatar"
+          alt="avatar"
+          src={`https://unavatar.io/${userName}`}
+        />
+        <div className="tw-followCard-info">
+          <strong>{children}</strong>
+          <span>{formatUsername(userName)}</span>
+        </div>
+      </header>
+      <aside>
+        <button className={followBtnClassName} onClick={handleClickBtn}>
+          {followText}
+        </button>
+      </aside>
+    </article>
+  );
+}
